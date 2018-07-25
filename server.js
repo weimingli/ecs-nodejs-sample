6f39bcd2f6cef31533903c8fee0d09aa4c1055d7
Weiming Li <weiming.li@lanyun.net>
2018-07-26 00:27:19 +0800

    Changed title

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 80;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('<h1>hahahahaha!!!!!</h1></br> <h2>Container IP Address:  ' + ip.address() + '</h2>');
+  res.send('<h1>Love DevOps<h1></br> <h2>Container IP Address:  ' + ip.address() + '</h2>');
 });
 
 app.listen(PORT);
